37d637259b6787e1e137ecff052d36b5e094bdf9
ArdaKestane <ardakestane@hotmail.com>
2023-02-01 00:43:51 +0300

    Added Card with props

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,21 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import './App.css'
+import Navbar from './Navbar.jsx'
+import Hero from './Hero.jsx'
+import Card from './Card.jsx'
+import "./style.css"
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-
+    <Navbar />
+    <Hero />
+    <Card
+      img="katie-zaferes.png"
+      rating="5.0"
+      reviewCount={6}
+      country="USA"
+      title="Life Lessons with Katie Zaferes"
+      price={136} ></Card>
   </React.StrictMode>,
 )
---
+++ src/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+
+export default function Main() {
+    return (
+        <nav>
+            <img className="nav-logo" src="src\Images\logo_name.png"></img>
+        </nav>
+
+    )
+}
